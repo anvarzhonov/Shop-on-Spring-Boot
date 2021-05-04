9c81d67c7fff93ca5b4ebc461f21d5ea9fc0b9d4
Anvarzhonov <anvarzhonov@mail.ru>
2021-05-04 22:48:59 +0300

    Добавили контроллеры для редактирования и добавления типов продукта

--- src/main/java/CourseWork/OnlineStore/controllers/MainController.java
+++ src/main/java/CourseWork/OnlineStore/controllers/MainController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.HashMap;
 import java.util.List;
@@ -46,4 +49,40 @@ public class MainController {
         return "products";
     }
 
+    /* -------------------------Контроллеры для редактипования и добавления типов продукта-------------------------*/
+    @GetMapping("/productTypeList")
+    public String productTypeList(Model model) {
+        Iterable<ProductType> types = productTypeRepository.findAll();
+        model.addAttribute("types", types);
+        return "productTypeList";
+    }
+
+    @GetMapping("productTypeList/add")
+    public String productTypeListAdd(Model model) {
+        ProductType productType = new ProductType();
+        model.addAttribute("productType", productType);
+        return "productTypeForm";
+    }
+
+    @PostMapping("productTypeList/add")
+    public String productTypeListAddSubmit(@ModelAttribute ProductType productType, Model model){
+        productTypeRepository.save(productType);
+        model.addAttribute("types", productTypeRepository.findAll());
+        return "productTypeList";
+    }
+
+    @GetMapping("/productTypeList/delete/{productTypeId}")
+    public String productTypeListDelete(@PathVariable("productTypeId") long id, Model model) {
+        productTypeRepository.deleteById(id);
+        model.addAttribute("types", productTypeRepository.findAll());
+        return "productTypeList";
+    }
+
+    @GetMapping("/productTypeList/edit/{productTypeId}")
+    public String productTypeListEdit(@PathVariable("productTypeId") long id, Model model) {
+        ProductType productType = productTypeRepository.findById(id).orElse(null);
+        model.addAttribute("productType", productType);
+        return "productTypeForm";
+    }
+    /*==============================================================================================================*/
 }
